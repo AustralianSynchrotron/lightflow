e85840b2ffc06140c08ad183df49e94fc65b5739
Stephen Mudie <Stephen.Mudie@synchrotron.org.au>
2016-08-30 12:23:08 +1000

    Completes features of chunking_task

--- examples/chunking_dag.py
+++ examples/chunking_dag.py
@@ -5,7 +5,7 @@ from lightflow.tasks import PythonTask, ChunkingTask
 def make_list(name, data, data_store, signal):
     print(name)
     data['my_list'] = ['asdf_0001.dat', 'asdf_0002.dat', 'sdfa_0001.dat', 'sdfa_0002.dat', 'sdfa_0003.dat',
-                       'blah_0001.dat', '|', 'blah_0002.dat', 'blah2_0001.dat', '|']
+                       'blah_0001.dat', '|', 'blah_0002.dat', 'blah2_0001.dat']
     return Action(data)
 
 
@@ -28,7 +28,7 @@ print_dag.define({print_list_task: []})
 chunk_dag = Dag('chunk_dag')
 make_list_task = PythonTask(name='make_list',
                             python_callable=make_list)
-chunk_task = ChunkingTask(name='print_me', dag_name='print_dag',
-                          pattern='(?P<match>[0-9A-Za-z]*)_', key='my_list')
+chunk_task = ChunkingTask(name='chunk_me', dag_name='print_dag', force_consecutive=True, flush_on_end=False,
+                          match_pattern='(?P<match>[0-9A-Za-z]*)_', key='my_list')
 
 chunk_dag.define({make_list_task: chunk_task})
--- lightflow/tasks/chunking_task.py
+++ lightflow/tasks/chunking_task.py
@@ -1,24 +1,63 @@
 import re
-from lightflow.tasks import TriggerTask
+from lightflow.models import BaseTask
 from lightflow.models import Action
 
 
-class ChunkingTask(TriggerTask):
+def indices(lst, element, value=False):
+    """ Returns the indices, or optionally the value, for all occurrences of 'element' in 'lst'.
+
+    Args:
+        lst (list): List to search.
+        element:  Element to find.
+        value: If True return the list value instead of the element.
+
+    Returns:
+        list: List of indices or values
+    """
+    result = []
+    offset = -1
+    while True:
+        try:
+            offset = lst.index(element, offset+1)
+        except ValueError:
+            return result
+        result.append(offset if not value else lst[offset])
+
+
+class ChunkingTask(BaseTask):
     """ The Chunking task divides up lists based on patterns. """
 
-    def __init__(self, name, dag_name, key=None, pattern=None, decimate=1, force_run=False, propagate_skip=True):
+    def __init__(self, name, dag_name, key, match_pattern,
+                 flush_string='|', flush_on_end=True,
+                 force_consecutive=True, decimate=1,
+                 force_run=False, propagate_skip=True):
         """ Initialise the Chunking task.
 
         Args:
             name (str): The name of the task.
-            key: Key to the data object containing list to chunk.
-            pattern: The pattern used to match like entries.
+            dag_name (str): Dag to run with eat chunk.
+            key: Key to the data object containing list to chunk, and to overwrite with chunk.
+            match_pattern (str): The regex pattern used to match like entries. Pattern will be used with
+            re.search. Capture groups can be used. If the groups are unnamed the first group is used,
+            if a group is named "match" then it will be used.
+            flush_string (str): Flush the current chunk if this string is encountered. Only applicable
+            in consecutive mode.
+            flush_on_end (bool): Flush the final chunk at end of list. Only applicable in consecutive mode.
+            force_consecutive (bool): If True only matching entries in list that are consecutive in the list
+            will be chunked together. I.e., matching entries that are separated by another valid match will
+            end up in different chunks.
+            decimate (int): Decimate chunks by this factor. E.g., a decimate value of 2 will only keep every second
+            element in the chunk.
             force_run (bool): Run the task even if it is flagged to be skipped.
             propagate_skip (bool): Propagate the skip flag to the next task.
         """
-        super().__init__(name, dag_name, force_run, propagate_skip)
+        super().__init__(name, force_run, propagate_skip)
+        self._dag_name = dag_name
         self._key = key
-        self._pattern = pattern
+        self._pattern = match_pattern
+        self._flush_string = flush_string
+        self._flush_on_end = flush_on_end if force_consecutive is True else True
+        self._force_consecutive = force_consecutive
         self._decimate = decimate
 
     def run(self, data, data_store, signal, **kwargs):
@@ -50,41 +89,44 @@ class ChunkingTask(TriggerTask):
                 return
             new_list += saved_list
 
-            for num, item in enumerate(new_list):
-                match = re.search(self._pattern, item)
-                try:
-                    previous = match.groupdict()['match']
-                except KeyError:
-                    previous = match.group(0)
-                except AttributeError:
-                    continue
-
-                chunked_list = [[item]]
-                break
-
-            for item in new_list[num + 1:]:
-                if item == '|':
-                    current = item
-                else:
-                    match = re.search(self._pattern, item)
+            matches = []
+            if '?P' in self._pattern:
+                for item in new_list:
                     try:
-                        current = match.groupdict()['match']
-                    except KeyError:
-                        current = match.group(0)
+                        matches.append(re.search(self._pattern, item).groupdict()['match'])
                     except AttributeError:
+                        matches.append(None)
+            else:
+                for item in new_list:
+                    try:
+                        matches.append(re.search(self._pattern, item).groups(0)[0])
+                    except AttributeError:
+                        matches.append(None)
+
+            if self._force_consecutive:
+                previous = None
+                chunked_list = []
+                for num, item in enumerate(matches):
+                    if new_list[num] == self._flush_string:
+                        if num + 1 < len(matches):
+                            chunked_list.append([])
+                            previous = matches[num + 1]
                         continue
-
-                if current == previous:
-                    chunked_list[-1].append(item)
-                else:
-                    if item != '|':
-                        chunked_list.append([item])
+                    elif item is None:
+                        continue
+                    elif item != previous:
+                        chunked_list.append([new_list[num]])
+                        previous = item
                     else:
-                        pass
+                        chunked_list[-1].append(new_list[num])
 
-                previous = current
+                if not self._flush_on_end:
+                    data_store.set('chunking_list', chunked_list.pop())
 
-            data_store.set('chunking_list', chunked_list.pop())
+            else:
+                unique_matches = set(matches)
+                chunked_indices = [indices(matches, unique) for unique in unique_matches]
+                chunked_list = [[new_list[i] for i in cis] for cis in chunked_indices]
 
             for chunk in chunked_list:
                 data[self._key] = chunk[::self._decimate if self._decimate > len(chunk) else None]
