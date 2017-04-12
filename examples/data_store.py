80acf292853e2ecd005c064e953ee70e4a929008
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-04-12 17:50:09 +1000

    Add documentation to the data_store example

--- examples/data_store.py
+++ examples/data_store.py
@@ -1,49 +1,81 @@
+""" Keep data during a workflow run in the persistent data store
+
+Data that should be kept during a workflow run can be saved into the persistent
+data store. This data is deleted as soon as the workflow run ends, but is available
+to all tasks during the lifetime of the workflow.
+
+The data store provides methods to store and retrieve single values or append values
+to a list. This can even be done asynchronously from different tasks at the same time.
+
+The key under which the data is being stored supports a hierarchical structure using
+the dot notation.
+
+This workflow stores different types of data in the persistent data store and modifies
+them.
+
+"""
+
 from lightflow.models import Dag, Action
 from lightflow.tasks import PythonTask
 
 import numpy as np
 
 
-def first_call(data, store, signal, context):
+# the callable function to store data in the persistent data store. It stores a single
+# integer value in 'number', a single integer value into the hierarchical key
+# 'buffer' -> 'observable' and a numpy array into 'image'. Additionally it adds an integer
+# value to a list in 'sample' -> 'spectra'.
+def store_data(data, store, signal, context):
     store.set('number', 5)
     store.set('buffer.observable', 20)
-    store.push('sample.spectra', 7)
-    store.set('image', np.ones((10, 10)))
     store.set('image', np.ones((100, 100)))
+    store.push('sample.spectra', 7)
 
-    return Action(data)
 
-
-def second_call(data, store, signal, context):
+# the callable function for the task that retrieves and prints the 'number' and 'image'
+# values then modifies the 'number' value and creates a new list of 'filenames'.
+def modify_data(data, store, signal, context):
     number = store.get('number')
-    img = store.get('image')
-    print(img.shape)
+    print('The number is: {}'.format(number))
 
-    store.set('number', number*10)
+    img = store.get('image')
+    print('The dimension of the image is: {}'.format(img.shape))
+
+    store.set('number', number * 10)
     store.push('filenames', 'file_a.spec')
 
 
-def third_a_call(data, store, signal, context):
+# the callable function for the task that adds another filename to the list.
+def add_filename(data, store, signal, context):
     store.push('filenames', 'file_b.spec')
 
 
-def third_b_call(data, store, signal, context):
+# the callable function for the task that adds a nested list to the list of filenames and
+# then extends the list of filenames with two more entries.
+def add_more_filenames(data, store, signal, context):
     store.push('filenames', ['nested_a', 'nested_b'])
     store.extend('filenames', ['file_c.spec', 'file_d.spec'])
 
 
-d = Dag('myDag')
+# create the main DAG
+d = Dag('main_dag')
 
-first = PythonTask(name='first',
-                   callable=first_call)
+# create the tasks that call the functions above
+store_task = PythonTask(name='store_task',
+                        callable=store_data)
 
-second = PythonTask(name='second',
-                    callable=second_call)
+modify_task = PythonTask(name='modify_task',
+                         callable=modify_data)
 
-third_a = PythonTask(name='third_a',
-                     callable=third_a_call)
+add_filename_task = PythonTask(name='add_filename_task',
+                               callable=add_filename)
 
-third_b = PythonTask(name='third_b',
-                     callable=third_b_call)
+add_more_filename_task = PythonTask(name='add_more_filename_task',
+                                    callable=add_more_filenames)
 
-d.define({first: [second], second: [third_a, third_b]})
+# set up the graph of the DAG, in which the store_task and modify_task are called
+# in sequence while the add_filename_task and add_more_filename_task are run in parallel.
+d.define({
+    store_task: modify_task,
+    modify_task: [add_filename_task, add_more_filename_task]
+})
