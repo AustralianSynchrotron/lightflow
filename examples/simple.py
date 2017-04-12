719196ef244213ea216ea47b3943c82c1f194493
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-04-12 10:37:10 +1000

    Add comments to the simple example

--- examples/simple.py
+++ examples/simple.py
@@ -1,33 +1,39 @@
-""" Very simple workflow with two consecutive tasks. 
+""" Simple workflow of two tasks exchanging data. 
 
+The first task (put_task) stores the value 5 in the key 'value', that is then read
+and displayed by the second task (print_task).
 """
 
 from lightflow.models import Dag, Action
 from lightflow.tasks import PythonTask
 
 
-def put_data_me(data, store, signal, context):
-    print(context.task_name)
-    print(context.dag_name)
-    print(context.workflow_name)
-    print(context.workflow_id)
+# the callable function for the task that stores the value 5
+def put_data(data, store, signal, context):
+    print('Task {task_name} being run in DAG {dag_name} '
+          'for workflow {workflow_name} ({workflow_id})'.format(**context.to_dict()))
+
     data['value'] = 5
     return Action(data)
 
 
+# the callable function for the task that prints the data
 def print_value(data, store, signal, context):
-    print(context.task_name)
-    print(data['value'])
+    print('The value is: {}'.format(data['value']))
 
 
-d = Dag('myDag')
+# create the main DAG
+d = Dag('main_Dag')
 
-put_me = PythonTask(name='put_me',
-                    callable=put_data_me)
+# create the two tasks for storing and retrieving data
+put_task = PythonTask(name='put_task',
+                      callable=put_data)
 
-print_me = PythonTask(name='print_me',
-                      callable=print_value)
+print_task = PythonTask(name='print_task',
+                        callable=print_value)
 
+# set up the graph of the DAG, in which the put_task has to be executed first,
+# followed by the print_task.
 d.define({
-    put_me: print_me
+    put_task: print_task
 })
